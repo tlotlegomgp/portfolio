843d4d9604665b4425db7c33e905e1b546c626d8
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2020-12-22 14:48:03 +0200

    Changed the portfolio styling

--- index/views.py
+++ index/views.py
@@ -5,21 +5,22 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
+
 def home_view(request):
-    #If user has filled the email form and wants to post it
-	if request.method == "POST":
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			subject = form.cleaned_data["subject"]
-			email = form.cleaned_data["email"]
-			message = form.cleaned_data["message"]
+    # If user has filled the email form and wants to post it
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data["subject"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
 
-            #Process Email send
-			send_mail(subject, message, email, ['tlotlegomgp@gmail.com'])
-			return render(request, 'index/email_sent.html')
+# Process Email send
+            send_mail(subject, message, email, ['tlotlegomgp@gmail.com'])
+            return render(request, 'index/email_sent.html')
 
-	#Else Present empty form to user
-	else:
-		form = ContactForm()
-		
-	return render(request, 'index/index.html', {'form': form})
+    # Else Present empty form to user
+    else:
+        form = ContactForm()
+
+    return render(request, 'index/index.html', {'form': form})
