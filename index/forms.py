06d66761c70ba894e61c49f0c43d34f9f1b74e69
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-02-01 17:05:36 +0200

    Updated website styling

--- index/forms.py
+++ index/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=35, widget=forms.TextInput(
-        attrs={'class': "form-control", 'name': "subject", 'required': "", 'placeholder': "Subject", 'style': 'background-color: #2a2b2e;'}))
+        attrs={'class': "form-control", 'name': "subject", 'required': "", 'placeholder': "Subject", 'style': 'background-color: #2a2b2e; color: white;'}))
     email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={
-                             'class': "form-control", 'name': "email", 'required': "", 'placeholder': "Your Email", 'style': 'background-color: #2a2b2e;'}))
+                             'class': "form-control", 'name': "email", 'required': "", 'placeholder': "Your Email", 'style': 'background-color: #2a2b2e; color: white;'}))
     message = forms.CharField(max_length=250, widget=forms.Textarea(
-        attrs={'class': "form-control", 'name': "message", 'required': "", 'placeholder': "Message", 'style': 'background-color: #2a2b2e;'}))
+        attrs={'class': "form-control", 'name': "message", 'required': "", 'placeholder': "Message", 'style': 'background-color: #2a2b2e; color: white;'}))
