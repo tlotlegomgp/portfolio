86f33af9c9fa6589bd9e0dea790d5c39bb8a1527
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-02-01 12:30:06 +0200

    Changed theme colour

--- index/forms.py
+++ index/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=35, widget=forms.TextInput(
-        attrs={'class': "form-control", 'name': "subject", 'required': "", 'placeholder': "Subject", 'style': 'background-color: #2a2b2e; color: rgb(170, 209, 205);'}))
+        attrs={'class': "form-control", 'name': "subject", 'required': "", 'placeholder': "Subject", 'style': 'background-color: #2a2b2e;'}))
     email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={
-                             'class': "form-control", 'name': "email", 'required': "", 'placeholder': "Your Email", 'style': 'background-color: #2a2b2e; color: rgb(171, 209, 205);'}))
+                             'class': "form-control", 'name': "email", 'required': "", 'placeholder': "Your Email", 'style': 'background-color: #2a2b2e;'}))
     message = forms.CharField(max_length=250, widget=forms.Textarea(
-        attrs={'class': "form-control", 'name': "message", 'required': "", 'placeholder': "Message", 'style': 'background-color: #2a2b2e; color: rgb(171, 209, 205);'}))
+        attrs={'class': "form-control", 'name': "message", 'required': "", 'placeholder': "Message", 'style': 'background-color: #2a2b2e;'}))
