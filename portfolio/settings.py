9cf06b14bf96f9104663d4fd037a08206f620dc6
Tlotlego <tlotlegomgp@gmail.com>
2021-02-12 12:18:57 +0200

    Changed Debug value

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE', ' ') == 'True'
 
 ALLOWED_HOSTS = ['tlotlegomgp.herokuapp.com', '127.0.0.1']
 
