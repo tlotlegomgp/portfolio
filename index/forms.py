f018994bb961cf601f62f55c6f908f6b3374ca08
Tlotlego <tlotlegomgp@gmail.com>
2021-08-11 10:09:33 +0200

    Updated the user form

--- index/forms.py
+++ index/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=35, widget=forms.TextInput(
-        attrs={'class': "form-control", 'name': "subject", 'required': "", 'placeholder': "Subject", 'style': 'background-color: rgb(52,58,64); color: white;'}))
+        attrs={'class': "form-control", 'name': "subject", 'placeholder': "Subject", 'style': 'background-color: rgb(52,58,64); color: white;'}), required=True)
     email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={
-                             'class': "form-control", 'name': "email", 'required': "", 'placeholder': "Your Email", 'style': 'background-color: rgb(52,58,64); color: white;'}))
-    message = forms.CharField(max_length=250, widget=forms.Textarea(
-        attrs={'class': "form-control", 'name': "message", 'required': "", 'placeholder': "Message", 'style': 'background-color: rgb(52,58,64); color: white;'}))
+                             'class': "form-control", 'name': "email", 'placeholder': "Your Email", 'style': 'background-color: rgb(52,58,64); color: white;'}), required=True)
+    message = forms.CharField(max_length=600, widget=forms.Textarea(
+        attrs={'class': "form-control", 'name': "message", 'placeholder': "Message", 'style': 'background-color: rgb(52,58,64); color: white;'}), required=True)
